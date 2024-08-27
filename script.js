bce35e14bce0f3b7d1aea03177dad845b1f87d9c
inzam01 <shamasmarty@gmail.com>
2024-08-27 12:35:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const socket = io()
+
+//Connect to Server
 socket.on('connect', () => {
     console.log('Connected ' + socket.id)
-})
+});
 
+//Message received
 socket.on('chat_rcvd', (data) => {
     $('#chats').append(
         $('<li>').text(
             `${data.username}: ${data.msg}`
         )
-    )
-})
+    );
+});
 
 $(() => {
-    $('#chatbox').hide()
+    $('#chatbox').hide();
 
+    //Login button clicked
     $('#login').click(() => {
         socket.emit('login', {
             username: $('#username').val()
-        })
-    })
+        });
+    });
 
+    //Login Successfully
     socket.on('loggedin', () => {
-        console.log('Login successful')
+        console.log('Login successful');
         $('#loginform').hide()
         $('#chatbox').show()
-    })
+    });
 
+    //Send button clicked
     $('#send').click(() => {
-        console.log('Sending chat')
+        console.log('Sending chat');
+        const messageInput = $('#msg');
         socket.emit('chat', {msg: $('#msg').val()})
-    })
-})
+
+          // Clear message input box
+          messageInput.val('');
+    });
+});
